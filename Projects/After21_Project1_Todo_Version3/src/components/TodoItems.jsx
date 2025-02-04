e71bfcdbebd76dffa98e963ad9b355137688902a
LISTED MILE <listedmile@gmail.com>
2025-02-04 17:09:28 +0530

    Todo Complete

--- Projects/After21_Project1_Todo_Version3/src/components/TodoItems.jsx
+++ Projects/After21_Project1_Todo_Version3/src/components/TodoItems.jsx
@@ -5,7 +5,12 @@ const TodoItems = ({ todoItems,handleDeleteItem }) => {
   return (
     <>
           <div className={styles.itemsContainer}>
-        {todoItems.map(item => <TodoItem todoDate={item.dueDate} todoName={item.name} handleDeleteItem={handleDeleteItem} />)}
+        {todoItems.map(item => <TodoItem
+          todoDate={item.dueDate}
+          todoName={item.name}
+          handleDeleteItem={handleDeleteItem}
+          key={item.name}
+        />)}
         
       </div>
     </>
